53fe9dd61a3b3fd5975868041931f59125528683
Vladislav0410 <Vladkovalev0410@gmail.com>
2021-03-02 14:11:10 +0300

    Ended modal window plagin

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-const modal = $.modal()
+const modal = $.modal({
+    title: 'My Modal',
+    closable: true,
+    content:`
+            <p>load</p>`,
+    width: '400px' ,
+    footerButtons: [
+        {text: 'Ok', type: 'primary', handler() {
+            console.log('Primary btn clicked')
+            modal.close()
+        }},
+        {text: 'Cancel', type: 'danger', handler() {
+            console.log('Danger btn clicked')
+            modal.close()
+        }}
+    ]
+})
